b6afa949681f64b97bfe0a666713cc0ad341eddb
Mikhail Kormyshov <kormyshov@gmail.com>
2019-10-06 16:28:17 +0300

    Create listing2.cpp

---
+++ part1/chapter2/listing2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cstdio>
+#include <cstring>
+#include <memory.h>
+#include <cmath>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <set>
+#include <map>
+#include <list>
+#include <bitset>
+#include <algorithm>
+#define PI (3.1415926535897932384626433832795)
+#define INF (2147483647)
+#define LLINF (9223372036854775807LL)
+#define EPS (1e9)
+#define MOD (1000000007)
+#define y1 stupid_cmath
+#define y0 stupid_cmath_too
+using namespace std;
+typedef long long LL;
+const int N = 1e5+9;
+int main(){
+    freopen("", "r", stdin);
+    freopen("", "w", stdout);
+    return 0;
+}
