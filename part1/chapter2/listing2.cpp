4491e3c45f7e5bffdb2d9d4e0686becc8263aeab
Mikhail Kormyshov <kormyshov@gmail.com>
2019-10-06 16:29:58 +0300

    Update listing2.cpp

--- part1/chapter2/listing2.cpp
+++ part1/chapter2/listing2.cpp
@@ -5,6 +5,7 @@
 #include <cstring>
 #include <memory.h>
 #include <cmath>
+
 #include <vector>
 #include <queue>
 #include <deque>
@@ -14,18 +15,26 @@
 #include <list>
 #include <bitset>
 #include <algorithm>
+
 #define PI (3.1415926535897932384626433832795)
 #define INF (2147483647)
 #define LLINF (9223372036854775807LL)
 #define EPS (1e9)
+
 #define MOD (1000000007)
+
 #define y1 stupid_cmath
 #define y0 stupid_cmath_too
+
 using namespace std;
+
 typedef long long LL;
+
 const int N = 1e5+9;
+
 int main(){
     freopen("", "r", stdin);
     freopen("", "w", stdout);
+    
     return 0;
 }
